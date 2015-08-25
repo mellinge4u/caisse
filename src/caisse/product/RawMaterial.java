25a496fcd0e9c3a6ee59514648538545320962de
Desmero <raphael.Merkling@gmail.com>
2015-08-25 20:45:37 +0200

    RawMaterial ajout des fonction de restock

--- src/caisse/product/RawMaterial.java
+++ src/caisse/product/RawMaterial.java
@@ -5,11 +5,15 @@ public class RawMaterial {
 	protected String name;
 	protected int stock;
 	protected double averageCost; // TODO Gérer le calcul du prix moyen
+	private int restock; // Utilisé pour calculer le prix moyen
+	private double restockCost; // Utilisé pour calculer le prix moyen
 
 	public RawMaterial(String name) {
 		this.name = name;
 		this.stock = 0;
 		this.averageCost = 0;
+		this.restock = 0;
+		this.restockCost = 0;
 	}
 
 	public String getName() {
@@ -36,4 +40,16 @@ public class RawMaterial {
 		return averageCost;
 	}
 
+	public void restock(int number, double price) {
+		restock += number;
+		stock += number;
+		restockCost += price;
+	}
+
+	public void endRestock() {
+		averageCost = restockCost / restock;
+		restock = 0;
+		restockCost = 0;
+	}
+	
 }
