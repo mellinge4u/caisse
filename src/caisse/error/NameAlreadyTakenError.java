30ed0deb8c8775b44c97feafc560d8cc6e75cdbb
Desmero <raphael.Merkling@gmail.com>
2015-08-25 20:11:20 +0200

    Ajout d'un erreur si un nom est deja utilisé

---
+++ src/caisse/error/NameAlreadyTakenError.java
@@ -0,0 +1,15 @@
+package caisse.error;
+
+public class NameAlreadyTakenError extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NameAlreadyTakenError(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
