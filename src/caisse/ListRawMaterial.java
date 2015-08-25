4d97db007280631444fd0f4ac12de84f6c7e05c8
Raphaël <raphael.Merkling@gmail.com>
2015-08-25 16:53:18 +0200

    Ajout des classes intermédiaire entre le model et les produits Ajout des classes :  - ListPurchasedProd  - ListRawMAterial  - ListSoldProd

---
+++ src/caisse/ListRawMaterial.java
@@ -0,0 +1,13 @@
+package caisse;
+
+import java.util.ArrayList;
+
+public class ListRawMaterial {
+
+	protected ArrayList<RawMaterial> list;
+	
+	public ListRawMaterial() {
+		this.list = new ArrayList<RawMaterial>();
+	}
+
+}
---
+++ src/caisse/ListSoldProd.java
@@ -0,0 +1,13 @@
+package caisse;
+
+import java.util.ArrayList;
+
+public class ListSoldProd {
+
+	protected ArrayList<SoldProduct> list;
+	
+	public ListSoldProd() {
+		this.list = new ArrayList<SoldProduct>();
+	}
+
+}
