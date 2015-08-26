a1ae45ab77165f99d6cf2ec799c48e601fb3f33c
Raphaël <raphael.Merkling@gmail.com>
2015-08-26 14:47:45 +0200

    Ajout du boutton d'ajout d'une matiere

--- src/caisse/view/StockView.java
+++ src/caisse/view/StockView.java
@@ -1,8 +1,12 @@
 package caisse.view;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import caisse.Model;
@@ -10,10 +14,21 @@ import caisse.Model;
 public class StockView extends JPanel implements Observer {
 
 	protected Model model;
+	protected JButton addMaterial;
 	
 	public StockView(Model model) {
 		this.model = model;
 		model.addObserver(this);
+		addMaterial = new JButton("Ajouter un produit");
+		addMaterial.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String s = (String)JOptionPane.showInputDialog(null, "Nom du produit : ", "Nouveau produit", -1);
+				model.addRawMaterial(s);
+			}
+		});
+		this.add(addMaterial);
 	}
 
 	@Override
