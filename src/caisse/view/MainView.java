9149ea18129b0a41e34348424f01f972b1040c81
Raphaël <raphael.Merkling@gmail.com>
2015-08-26 14:07:20 +0200

    Implémentation des nouvelles vues

--- src/caisse/view/MainView.java
+++ src/caisse/view/MainView.java
@@ -13,9 +13,9 @@ public class MainView extends JFrame{
 		jtb1 = new JTabbedPane();
 		jtb1.add("Vente",new SellView());
 		jtb1.add("Restockage",new RestockageView());
-		jtb1.add("Stock",null);
-		jtb1.add("Utilisateur",null);
-		jtb1.add("Historique",null);
+		jtb1.add("Stock",new StockView());
+		jtb1.add("Utilisateur",new UserView());
+		jtb1.add("Historique",new HistoricView());
 		this.add(jtb1);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		pack() ;
