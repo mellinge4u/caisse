662825879e23fb7ac45295e09e11d4044656aaf7
erwan <mellingererwan@gmail.com>
2015-08-28 13:49:15 +0200

    gestion des utilisateurs

--- src/caisse/member/Users.java
+++ src/caisse/member/Users.java
@@ -2,9 +2,18 @@ package caisse.member;
 
 import java.util.ArrayList;
 
-public class Users {
+import javax.swing.table.AbstractTableModel;
+
+import caisse.product.RawMaterial;
+
+public class Users extends AbstractTableModel{
 
 	private ArrayList<User> users;
+	protected String[] colNames = { "ID", "Nom", "Prenom", "Solde" };
+	protected Class<?>[] colClass = { Integer.class, String.class,
+			String.class,Integer.class };
+	protected Boolean[] colEdit = { false, false, false,false };
+	
 	
 	public Users(){
 		this.users = new ArrayList<>();
@@ -50,6 +59,34 @@ public class Users {
 	
 	private User NonExistingUserException() {
 		
+		return null;
+	}
+
+	@Override
+	public int getRowCount() {
+		return users.size();
+	}
+
+	@Override
+	public int getColumnCount() {
+		return colNames.length;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		
+		switch (columnIndex) {
+		case 0:
+			return users.get(rowIndex).getUserNumber();
+		case 1:
+			return users.get(rowIndex).getName();
+		case 2:
+			return users.get(rowIndex).getFirstname();
+		case 3:
+			return ((double) users.get(rowIndex).getAcount() / 100);
+		default:
+			break;
+		}
 		return null;
 	}
 	
--- src/caisse/view/UserView.java
+++ src/caisse/view/UserView.java
@@ -1,18 +1,38 @@
 package caisse.view;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
 
 import caisse.Model;
+import caisse.member.Users;
 
 public class UserView extends JPanel implements Observer{
 
-	protected Model modlel;
+	protected Model model;
+	protected JTable usersTable;
+	protected JButton addUser;
+	protected Users users;
 	
-	public UserView(Model model) {
-		this.modlel = model;
+	public UserView(final Model model) {
+		this.model = model;
+		users = new Users();
+		this.usersTable = new JTable(users);
+		addUser = new JButton("ajouter un nouvel utilisateur");
+		addUser.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new AddUserView(model);
+			}
+		});
 		model.addObserver(this);
 	}
 
