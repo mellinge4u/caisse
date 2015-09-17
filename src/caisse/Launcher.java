66a92a1843958e04f0ab967b1207b7a06cd3301d
Raphaël <raphael.Merkling@gmail.com>
2015-09-17 09:40:37 +0200

    Changement birthDate dans le launcher

--- src/caisse/Launcher.java
+++ src/caisse/Launcher.java
@@ -1,5 +1,6 @@
 package caisse;
 
+import java.text.ParseException;
 import java.util.Date;
 
 import caisse.file.ReadFile;
@@ -17,7 +18,12 @@ public class Launcher {
 		ReadFile.readHistoric(model);
 
 		// Users
-		model.addUser(150001, "Merkling", "Raphael", true, new Date(), "M1 info", "41 rue Sainte Catherine", "54000", "Nancy", "raphael.merkling@laposte.net", true);
+		try {
+			model.addUser(150001, "Merkling", "Raphael", true, User.df.parse("27/12/1991"), "M1 info", "41 rue Sainte Catherine", "54000", "Nancy", "raphael.merkling@laposte.net", true);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		model.addUser(0, "...", "...", true, new Date(), "", "", "", "", "", false);
 		model.addUser(150003, "Matuang", "Pierre", true, new Date(), "", "", "", "", "", false);
 		model.addUser(150007, "Marmier", "Herve", true, new Date(), "", "", "", "", "", false);
